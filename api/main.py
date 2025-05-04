8036aa7333b90ec07b0f8f954f08359769af925d
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2025-05-04 08:50:12 +0200

    feat: implement API endpoints to access DB

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
+from db import Database
 from fastapi import FastAPI
+
+db = Database()
 
 app = FastAPI()
 
@@ -6,3 +9,71 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/position/{position_hash}")
+async def get_position(position_hash: int):
+    position = db.get_position(position_hash)
+    if position:
+        return {
+            "positionID": position["positionID"],
+            "timesPlayed": position["timesPlayed"],
+            "whiteWins": position["whiteWins"],
+            "blackWins": position["blackWins"],
+            "recursiveScoreWhite": position["recursiveScoreWhite"],
+            "recursiveScoreBlack": position["recursiveScoreBlack"],
+        }
+    return {"error": "Position not found"}
+
+
+@app.get("/position/{position_hash}/moves")
+async def get_next_moves(position_hash: int):
+    moves = db.get_next_moves(position_hash)
+    if moves:
+        return [
+            {
+                "positionID": move["positionID"],
+                "timesPlayed": move["timesPlayed"],
+                "whiteWins": move["whiteWins"],
+                "blackWins": move["blackWins"],
+                "recursiveScoreWhite": move["recursiveScoreWhite"],
+                "recursiveScoreBlack": move["recursiveScoreBlack"],
+                "move_times_played": move["move_times_played"],
+            }
+            for move in moves
+        ]
+    return {"error": "No moves found for this position"}
+
+
+@app.get("/fen/{fen}/{rating}/position")
+async def get_position_by_fen(fen: str, rating: int):
+    position = db.get_position_by_fen(fen, rating)
+    if position:
+        return {
+            "positionID": position["positionID"],
+            "timesPlayed": position["timesPlayed"],
+            "whiteWins": position["whiteWins"],
+            "blackWins": position["blackWins"],
+            "recursiveScoreWhite": position["recursiveScoreWhite"],
+            "recursiveScoreBlack": position["recursiveScoreBlack"],
+        }
+    return {"error": "Position not found"}
+
+
+@app.get("/fen/{fen}/{rating}/position/moves")
+async def get_next_moves_by_fen(fen: str, rating: int):
+    moves = db.get_next_moves_by_fen(fen, rating)
+    if moves:
+        return [
+            {
+                "positionID": move["positionID"],
+                "timesPlayed": move["timesPlayed"],
+                "whiteWins": move["whiteWins"],
+                "blackWins": move["blackWins"],
+                "recursiveScoreWhite": move["recursiveScoreWhite"],
+                "recursiveScoreBlack": move["recursiveScoreBlack"],
+                "move_times_played": move["move_times_played"],
+            }
+            for move in moves
+        ]
+    return {"error": "No moves found for this position"}
