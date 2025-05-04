51771c75fe40c84ea578f3347b62bc76c5bbcea1
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2025-05-04 17:50:23 +0200

    feat: add chess pieces

--- apps/explorer/src/main.ts
+++ apps/explorer/src/main.ts
@@ -2,5 +2,11 @@
 // import Chessboard2 from "@chrisoakman/chessboard2";
 // import "@chrisoakman/chessboard2/dist/chessboard2.min.css";
 import { Chessboard2 } from "chessboard2";
+import "../node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.css";
 
-const board = Chessboard2("board");
+const config = {
+  PieceTheme: "img/chesspieces/wikipedia/{piece}.svg",
+  position: "start",
+};
+
+const board = Chessboard2("board", config);
