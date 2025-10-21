a5e70eb6364437f6d2658c3436108b20f55a369a
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2025-10-21 08:07:39 +0200

    fix

--- apps/explorer/src/main.ts
+++ apps/explorer/src/main.ts
@@ -1,4 +1,5 @@
 // main.js
+// Mithilfe von KI generiert
 // import Chessboard2 from "@chrisoakman/chessboard2";
 // import "@chrisoakman/chessboard2/dist/chessboard2.min.css";
 import { Chessboard2 } from "chessboard2";
